c10428c928021766766a3ada5c050f48691b5178
Shiy <siqgustavo2@hotmail.com>
2022-09-20 19:20:24 -0300

    participantes

---
+++ src/models/participants.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { v1: uuid } = require("uuid");
+
+const userSchema = mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+    default: uuid,
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phone:{
+    type: Number,
+    required: true,
+  }
+  
+})
+
+
+const user = mongoose.model("user", userSchema);
+
+module.exports = user;
