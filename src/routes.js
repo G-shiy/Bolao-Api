8c1ceb4420001a653399414273f36ebae4126652
Shiy <siqgustavo2@hotmail.com>
2022-09-22 11:08:42 -0300

    adicionando comentários nas futuras rotas

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,16 @@ const gamesController = require('./controllers/gamesController');
 
 const routes = express.Router();
 
+// Retorna todos os jogos disponíveis do bolão
 routes.get("/games", gamesController.store);
+// Retorna todos os jogos finalizados do bolão
+
+// Publica um novo jogo no bolão
 routes.post("/newGame", gamesController.create)
+
+// Deleta um jogo
+
+// Loga no SuperUsuário;
+
 
 module.exports = routes;
